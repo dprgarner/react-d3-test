d99dac591935efec12554c05fb68fb8fcb2fbadb
David Garner <dprgarner@gmail.com>
2016-12-22 15:11:57 +0000

    Add incrementor buttons

--- src/start.jsx
+++ src/start.jsx
@@ -1,11 +1,52 @@
 import {render} from 'react-dom'
 import {createStore} from 'redux';
-import {Provider} from 'react-redux';
+import {Provider, connect} from 'react-redux';
+
+const initialState = {things: []};
+
+let i = 0;
+
+function addThing() {
+  return {
+    type: 'add',
+    name: i++,
+  }
+}
+
+function reducer(state=initialState, action) {
+  switch (action.type) {
+    case 'add':
+      const newThing = {name: action.name}
+      return {...state, things: [...state.things, newThing]};
+  }
+  return state;
+}
+
+let Buttons = ({dispatch}) => (
+  <div>
+    <button onClick={() => dispatch(addThing())}>Add a thing</button>
+    <button>Remove a thing</button>
+  </div>
+);
+Buttons = connect()(Buttons);
+
+let Things = ({things}) => (
+  <ul>
+    {things.map(({name}, index) => <li key={index}>{name}</li>)}
+  </ul>
+);
+
+function mapThingsStateToProps({things}) {
+  return {things};
+}
+Things = connect(mapThingsStateToProps)(Things);
 
 const App = () => (
-  <div>{'Hello world!'}</div>
-)
-const reducer = (state, action) => state;
+  <div>
+    <Buttons />
+    <Things />
+  </div>
+);
 
 window.onload = () => {
   let store = createStore(reducer);
